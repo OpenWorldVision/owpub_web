c8e681cdc903b43aaf24da9878be271deda2a7d3
phuongnn <phuongnn>
2022-11-02 22:50:57 +0700

    add ignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   useEffect(() => {
+    // @ts-ignore
     window.ReactNativeWebView.postMessage("hahaha");
   }, []);
   return (
